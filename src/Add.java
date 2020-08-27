31d8d76f12272d654ffc95c2760a1f0a31d7e001
sanan90 <san-an@bk.ru>
2020-08-27 03:11:02 +0300

    HW3

--- src/Add.java
+++ src/Add.java
@@ -1,3 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Add {
+    String surname;
+    Integer number;
+
+    public Map<String, Integer> newNum = new HashMap<>();
+
+    public Add(String surname, Integer number) {
+        this.surname = surname;
+        this.number = number;
+        newNum.put(surname, number);
+    }
+
+    public Add (String surname) {
+        this.surname = surname;
+        System.out.println(newNum.get(surname));
+    }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,5 +1,40 @@
+ import com.sun.org.apache.xml.internal.utils.Hashtree2Node;
+
+import javax.swing.text.html.HTMLDocument;
+import java.util.*;
+
 public class main {
     public static void main(String[] args) {
-        
+
+//        Первое задание!!!!!
+//        String[] words = {
+//                "Пессимизм", "Злость" , "Бактерия" , "Бактерия",
+//                "программирование" , "Злость" , "программирование",
+//                "Бактерия" , "Птица" , "Погода", "Космос" , "Птица" ,
+//                "программирование", "Осознание" , "Осознание" ,
+//                "Школа", "Птица" , "Гауптвахта" , "Пессимизм",
+//                "Камин" , "программирование" , "Бактерия",
+//                "Школа" , "Пробел" , "Пессимизм", "Школа" ,
+//                "Гауптвахта" , "Осознание", "Космос" , "программирование"
+//        };
+//        Map<String, Integer> map = new HashMap<>();
+//        for (int i = 0; i < words.length; i++) {
+//            String oneWord = words[i];
+//            Integer word = map.getOrDefault(oneWord, 0);
+//            map.put(oneWord, word + 1);
+//        }
+//        System.out.println(map);
+
+
+        new Add("Мамедов", 7008070);
+        new Add("Панин", 7888778);
+        new Add("Трамп", 5887851);
+        new Add("Алиев", 1101010);
+        new Add("Путин", 1000001);
+        new Add("Лукашенко", 9989968);
+
+        new Get("Путин");
+
+
     }
 }
